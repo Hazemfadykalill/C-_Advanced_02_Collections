399b737cc043324231f60951738916e3fd909998
Hazem Fady <hazemfady123@gmail.com>
2024-08-01 19:03:17 +0300

    This Contain Quetions Body

---
+++ Assignment_Advanced_Collections/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection.Metadata;
+
+namespace Assignment_Advanced_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01
+            /*⮚	Write a Report about all Collections Given in the Session and Compare
+             * [Structure, Time Complexity, and Business Case With Implemented Examples]
+             */
+            #endregion
+
+            #region Part02
+            #region Q01
+            /*1.You are given an ArrayList containing a sequence of elements.
+             * try to reverse the order of elements in the ArrayList
+             * in-place(in the same arrayList) without using the built-in Reverse.
+             * Implement a function that takes the ArrayList as input and modifies it to have the reversed order of elements.
+             */
+            #endregion
+
+            #region Q02
+            /*2.You are given a list of integers. Your task is to find and return a new list 
+             * containing only the even numbers from the given list.
+             */
+
+            #endregion
+
+            #region Q03
+            /*3.implement a custom list called FixedSizeList<T> with a predetermined capacity.
+             * This list should not allow more elements than its capacity and should provide clear messages
+             * if one tries to exceed it or access invalid indices.
+             * Requirements:
+             */
+            //1.Create a generic class named FixedSizeList<T>.
+            //2.Implement a constructor that takes the fixed capacity of the list as a parameter.
+            //3.Implement an Add method that adds an element to the list, but throws an exception if the list is already full.
+            //4.Implement a Get method that retrieves an element at a specific index in the list but throws an exception for invalid indices.
+
+
+            #endregion
+
+             
+            #endregion
+        }
+    }
+}
